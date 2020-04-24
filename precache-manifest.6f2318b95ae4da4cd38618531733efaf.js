0c87f23cc869e4afc29d2d6c6f9f010eb5033269
Dustin Rasener <dustin@clearsoftware.com>
2020-04-24 10:15:33 -0400

    Updates

---
+++ precache-manifest.6f2318b95ae4da4cd38618531733efaf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9295c03d451ba008e2405daa1101b4b0",
+    "url": "/mtgcardeval/index.html"
+  },
+  {
+    "revision": "449758a41ff56dd08a71",
+    "url": "/mtgcardeval/static/css/2.080de803.chunk.css"
+  },
+  {
+    "revision": "f730e632c60284e72841",
+    "url": "/mtgcardeval/static/css/main.4676e452.chunk.css"
+  },
+  {
+    "revision": "449758a41ff56dd08a71",
+    "url": "/mtgcardeval/static/js/2.14769a95.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/mtgcardeval/static/js/2.14769a95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f730e632c60284e72841",
+    "url": "/mtgcardeval/static/js/main.12910716.chunk.js"
+  },
+  {
+    "revision": "daf7098d2dec718f47a8",
+    "url": "/mtgcardeval/static/js/runtime-main.73dfd485.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/mtgcardeval/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/mtgcardeval/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/mtgcardeval/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/mtgcardeval/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/mtgcardeval/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
