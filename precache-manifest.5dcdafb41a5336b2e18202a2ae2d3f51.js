390cf6af0098583649b02d290aecf6aae41ae756
Dustin Rasener <dustin@clearsoftware.com>
2020-05-11 21:48:38 -0400

    Updates

--- precache-manifest.5dcdafb41a5336b2e18202a2ae2d3f51.js
+++ precache-manifest.5dcdafb41a5336b2e18202a2ae2d3f51.js
@@ -1,31 +1,35 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8a45f64100510848300a70088baf3fd3",
+    "revision": "04d87cb2bb3d6d1cc522ca17ba9a9045",
     "url": "/mtgcardeval/index.html"
   },
   {
-    "revision": "99b2ce8cceeedcd902af",
+    "revision": "5d02950b82f6cf2e0228",
     "url": "/mtgcardeval/static/css/2.080de803.chunk.css"
   },
   {
-    "revision": "86f5454b3eb668072ae5",
-    "url": "/mtgcardeval/static/css/main.98edca37.chunk.css"
+    "revision": "51cc466ef053c7cd2790",
+    "url": "/mtgcardeval/static/css/main.31c801f4.chunk.css"
   },
   {
-    "revision": "99b2ce8cceeedcd902af",
-    "url": "/mtgcardeval/static/js/2.fb653a7c.chunk.js"
+    "revision": "5d02950b82f6cf2e0228",
+    "url": "/mtgcardeval/static/js/2.1520b141.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/mtgcardeval/static/js/2.fb653a7c.chunk.js.LICENSE.txt"
+    "url": "/mtgcardeval/static/js/2.1520b141.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "86f5454b3eb668072ae5",
-    "url": "/mtgcardeval/static/js/main.7a7e79e9.chunk.js"
+    "revision": "51cc466ef053c7cd2790",
+    "url": "/mtgcardeval/static/js/main.20ff9571.chunk.js"
   },
   {
     "revision": "daf7098d2dec718f47a8",
     "url": "/mtgcardeval/static/js/runtime-main.73dfd485.js"
+  },
+  {
+    "revision": "d0c4a4497ef4c11b161d19c9a3fbcc2f",
+    "url": "/mtgcardeval/static/media/call_to_action.d0c4a449.svg"
   },
   {
     "revision": "7c96c8965188131419fdf7b36db5eee7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mtgcardeval/precache-manifest.1c17a41dc9bc1189a9122ee6f8217660.js"
+  "/mtgcardeval/precache-manifest.5dcdafb41a5336b2e18202a2ae2d3f51.js"
 );
 
 self.addEventListener('message', (event) => {
